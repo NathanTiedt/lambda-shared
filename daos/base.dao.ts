51e2ccca3ec04928d2a97be75c58574d6ab8bd34
Nate Tiedt <ntiedt@shutterfly.com>
2019-12-11 12:35:50 -0700

    added transaction functions for dao

--- daos/base.dao.ts
+++ daos/base.dao.ts
@@ -7,20 +7,74 @@ export default class BaseDao {
   client: Client;
   connected: boolean = false;
   log: Logger;
-  transactionOpen: boolean = false;
+  transactionStarted: boolean = false;
 
+  /** @constructor **/
   construtor(className: string, client?: Client) {
     this.log = new Logger(className);
     this.client = client || new Client();
   }
 
+  /**
+   * begins a transaction
+   */
   public begin = async () => {
+    if (this.transactionStarted) 
+      this.log.warn(`Transaction already started`);
+    try {
+      this.log.debug(`Begining Transaction`);
+      await this.query(`BEGIN`);
+      return true;
+    } catch (error) {
+      this.log.error(`Transaction failed to begin`, error);
+      return false;
+    }
+  }
+
+  /**
+   * closes a database connection
+   */
+  public close = async () => {
+    if (this.transactionStarted) 
+      throw new Error(`Transaction currently open. Commit changes before closing connection`);
+    try {
+      this.log.debug(`Closing database connection`);
+      await this.client.close();
+      this.connected = false;
+      return true;
+    } catch (error) {
+      this.log.error(`Failed to close database connection`, error);
+      throw error;
+    }
+  }
+
+  /**
+   * commits a transaction
+   */
+  public commit = async () => {
+    if (!this.transactionStarted) 
+      throw new Error(`No transaction in progress`);
+    try {
+      this.log.debug(`Commiting transaction`);
+      this.transactionStarted = false; // set to false so the query function closes the connection
+      await this.query(`COMMIT`);
+      return true;
+    } catch (error) {
+      this.log.error(`Transaction failed to commit`, error);
+      return false;
+    }
+  }
+
+  /**
+   * connects to a database
+   */
+  public connect = async () => {
     if (!this.connected) {
       try {
         const start = Date.now();
         await this.client.connect();
         this.connected = true;
-        this.log.trace(`opened database connection in ${Date.now() - start} milliseconds`);
+        this.log.debug(`opened database connection in ${Date.now() - start} milliseconds`);
       } catch (error) {
         this.log.error(`Dao failed to connect to database`, error);
         this.connected = false;
@@ -29,28 +83,38 @@ export default class BaseDao {
     return this.connected;
   }
 
-  public close = async () => {
-    if (this.transactionOpen) 
-      throw new Error(`Transaction currently open. Commit changes before closing connection`);
+  /**
+   * queries the database
+   */
+  public query = async (query: string, params?: string[]) => {
     try {
-      return await this.client.close();
+      const start = Date.now();
+      await this.connect();
+      const result = await this.client.query(query, params);
+      this.log.debug(`Query Successful: ${result.rowCount} rows returned in ${Date.now() - start}`);
+      return result.rows;
     } catch (error) {
-      this.log.error(`Failed to close database connection`);
-      throw error;
+      this.log.error(`Querying database failed`, error);
+      throw new Error(error);
+    } finally {
+      if (!this.transactionStarted) this.close();
     }
   }
 
-  public query = async (paramaterizedQuery: string, params: string[]) => {
-    let result: any = {};
-    await this.begin();
+  /**
+   * rollsback a transaction
+   */
+  public rollback = async () => {
+    if (!this.transactionStarted) 
+      throw new Error(`No transaction in progress`);
     try {
-      const start = Date.now();
-      result = await this.client.query(paramaterizedQuery, params);
-      this.log.trace(`Query Successful: ${result.rowCount} rows returned in ${Date.now() - start}`);
+      this.log.debug(`Rolling back transaction`);
+      this.transactionStarted = false; // set to false so the query function closes the connection
+      await this.query(`ROLLBACK`);
+      return true;
     } catch (error) {
-      this.log.error(`Querying database failed`, error.stack);
-    } finally {
-      if (!this.transactionOpen) this.close();
+      this.log.error(`Transaction failed to rollback`, error);
+      return false;
     }
   }
 
